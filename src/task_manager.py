247dbf62edae9a943863b98cc87beaad91abd55b
Maedeh Kokabi <maekokabi@gmail.com>
2025-12-22 22:08:42 +0330

    prints omitted + searchbyatt function added

--- src/task_manager.py
+++ src/task_manager.py
@@ -12,7 +12,6 @@ class TaskManager:
 
         with open(filename, "w") as f:
             json.dump(data, f, indent=4)
-        print("Data saved.")
 
     def load_from_file(self, filename="tasks.json"):
         try:
@@ -20,118 +19,67 @@ class TaskManager:
                 data = json.load(f)
                 self.tasks = [Task.from_dict(task) for task in data["tasks"]]
         except FileNotFoundError:
-            print("No saved file found.")
+            raise ValueError("No saved file found.")
         except json.JSONDecodeError:
-            print("Json file is corrupted.")
+            raise ValueError("Json file is corrupted.")
         except KeyError:
-            print("Json structure missing.")
+            raise ValueError("Json structure missing.")
 
     def display_tasks(self):
-        if self.tasks == []:
-            print("No task entries.")
-        else:
-            for task in self.tasks:
-                print(task)
+        if not self.tasks:
+            raise ValueError("No task entries.")
+        return self.tasks
 
     def add_task(self, id:int, task, description="", deadline="No Deadline", done="Not done"):
         task_object = Task(id, task, description, deadline, done)
 
         if any(task.id == task_object.id for task in self.tasks):
-            print("This id already exists.")
+            raise ValueError("This id already exists.")
         else:
-            try:
-                task_object.validate_all()
-                self.tasks.append(task_object)
-                print("Task added.")
-                self.save_to_file()
-            except ValueError as e:
-                print(f"{e}")
-
-    def delete_task(self, id:int, task, description="", deadline="No Deadline", done="Not done"):
-        task_object = Task(id, task, description, deadline, done)
-
-        if not any(task_object == t for t in self.tasks):
-            print("Task not found.")
-        else:
-            try:
-                self.tasks.remove(task_object)
-                print("Task removed.")
-                self.save_to_file()
-            except ValueError as e:
-                print(f"{e}")
+            task_object.validate_all()
+            self.tasks.append(task_object)
+            self.save_to_file()
 
     def delete_task_by_id(self, task_id):
         if not any(t.id == task_id for t in self.tasks):
-            print("No task by this id.")
+            raise ValueError("No task by this id.")
         else:
-            try:
-                matched_task = next((t for t in self.tasks if t.id == task_id), None)
-                self.tasks.remove(matched_task)
-                print("Task removed.")
-            except ValueError as e:
-                print(f"{e}")
+            matched_task = next((t for t in self.tasks if t.id == task_id), None)
+            self.tasks.remove(matched_task)
 
-    def search_by_task(self, task):
-        if not any(t.task == task for t in self.tasks):
-            print("No exisiting task entries with this name.")
-        else:
-            try:
-                [print(t) for t in self.tasks if t.task == task]
-            except ValueError as e:
-                print(f"{e}")
-
-    def search_by_deadline(self, deadline):
-        if not any(t.deadline == deadline for t in self.tasks):
-            print("No task entries with this deadline.")
-        else:
-            try:
-                [print(t) for t in self.tasks if t.deadline == deadline]
-            except ValueError as e:
-                print(f"{e}")
+    def search_by_attribute(self, attr_name, value):
+        results = [e for e in self.expenses if getattr(e, attr_name) == value]
+        if not results:
+            raise ValueError(f"No entries found for {attr_name} = {value}")
+        return results
 
     def all_done_tasks(self):
-        if not any(t.status == "Done" for t in self.tasks):
-            print("No finished tasks.")
-        else:
-            try:
-                print("All finished tasks: ")
-                [print(t) for t in self.tasks if t.status == "Done"]
-            except ValueError as e:
-                print(f"{e}")
+        results = [t for t in self.tasks if t.status == "Done"]
+        if not results:
+            raise ValueError("No completed task.")
+        return results
 
     def all_not_done_tasks(self):
-        if not any(t.status == "Not done" for t in self.tasks):
-            print("All tasks are completed.")
-        else:
-            try:
-                print("All unfinished tasks: ")
-                [print(t) for t in self.tasks if t.status == "Not done"]
-            except ValueError as e:
-                print(f"{e}")
+        results = [t for t in self.tasks if t.status == "Not done"]
+        if not results:
+            raise ValueError("All tasks are completed.")
+        return results 
 
     def mark_task_done(self, task_id):
-        if not any(t.id == task_id for t in self.tasks):
-            print("Task doesn't exist.")
+        matched_task = next((t for t in self.tasks if t.id == task_id), None)
+        if not matched_task:
+            raise ValueError("Task doesn't exist.")
         else:
-            try:
-                matched_task = next((t for t in self.tasks if t.id == task_id), None)
-                matched_task.mark_done()
-                print("Task is completed.")
-                self.save_to_file()
-            except ValueError as e:
-                print(f"{e}")
+            matched_task.mark_done()
+            self.save_to_file()
 
     def mark_task_undone(self, task_id):
-        if not any(t.id == task_id for t in self.tasks):
-            print("Task doesn't exist.")
+        matched_task = next((t for t in self.tasks if t.id == task_id), None)
+        if not matched_task:
+            raise ValueError("Task doesn't exist.")
         else:
-            try:
-                matched_task = next((t for t in self.tasks if t.id == task_id), None)
-                matched_task.mark_undone()
-                print("Task is marked undone.")
-                self.save_to_file()
-            except ValueError as e:
-                print(f"{e}")
+            matched_task.mark_undone()
+            self.save_to_file()
 
 class Task:
     def __init__(self, id:int, task, description="", deadline="No Deadline", status="Not done"):
@@ -199,15 +147,11 @@ class Task:
     
     def mark_done(self):
         self.status = "Done"
-        print("Marked as done.")
 
     def mark_undone(self):
         if self.status == "Not done":
-            print("Task is already marked as undone.")
+            raise vars("Task is already marked as undone.")
         else:
-            try:
-                self.status = "Not done"
-                print("Marked as undone.")
-            except ValueError as e:
-                print(f"{e}")
+            self.status = "Not done"
+
     
